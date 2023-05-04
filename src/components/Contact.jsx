72af66a7a5b09ada205281ee125920f3ef421203
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-04 12:02:11 -0600

    contact form completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,6 +24,7 @@ function Contact() {
     return (
     <div class="contact-container" id="contact-section">
     <h2>Contact</h2>
+    <p>I would love to hear from you! You can message me directly below, email me at brooklyn.longhurst@gmail.com or contact me through <a href='https://www.linkedin.com/in/brooklyn-longhurst-bb1118268/' className='linkedIn-link'>LinkedIn.</a></p>
         <section class="">
             <div className='contact-form'>
                 <form ref={form} onSubmit={sendEmail}>
