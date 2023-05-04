42942cc0908835a614b68ef94217da5c74cb195a
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-04 11:49:36 -0600

    implements emailjs on contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm(
+        'service_mals9yp', 
+        'template_uebsgfm', 
+        form.current, 
+        'er-J8UbObwbhTz2Qf')
+        .then((result) => {
+            console.log(result.text);
+            console.log('message sent')
+            e.target.reset()
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+
     return (
     <div class="contact-container" id="contact-section">
     <h2>Contact</h2>
         <section class="">
             <div className='contact-form'>
-                <form>
-                    <input className="contact-inputs" type="text" placeholder='Name'/>
-                    <input className="contact-inputs" type='text' placeholder='Email' />
-                    <textarea className="contact-textarea" type='textarea' placeholder='Message' />
+                <form ref={form} onSubmit={sendEmail}>
+                    <input className="contact-inputs" type="text" name="user_name" placeholder='Name'/>
+                    <input className="contact-inputs" type='text' name='user_email' placeholder='Email' />
+                    <textarea className="contact-textarea" type='textarea' name='message' placeholder='Message' />
                     <div className='button-container'>
                     <button className='contact-button'>Send</button>
                     </div>
