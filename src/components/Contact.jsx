e8b501bc13bd3dd33fa63de9caf3b5149baabbbd
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-03 21:13:26 -0600

    contact inputs styled

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,18 +4,17 @@ function Contact() {
     return (
     <div class="contact-container" id="contact-section">
     <h2>Contact</h2>
-    <p>Let's get in touch.</p>
         <section class="">
             <div className='contact-form'>
                 <form>
                     <input className="contact-inputs" type="text" placeholder='Name'/>
                     <input className="contact-inputs" type='text' placeholder='Email' />
-                    <input className="contact-inputs" type='text' placeholder='Message' />
+                    <textarea className="contact-textarea" type='textarea' placeholder='Message' />
                 </form>
             </div>
-            <div>
-                <a href="https://www.linkedin.com/in/brooklyn-longhurst-bb1118268/"><img class="linkedin-icon" src="https://www.svgrepo.com/show/922/linkedin.svg" /></a>
-            </div>
+            {/* <div>
+                <a href="https://www.linkedin.com/in/brooklyn-longhurst-bb1118268/"><img class="linkedin-icon" src="https://www.svgrepo.com/show/922/linkedin.svg" alt='linkedin-icon'/></a>
+            </div> */}
         </section>
     </div>
     )
