dd185fc0418a8897fb2e2f421bc1d037dba26878
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-05-25 16:45:05 -0600

    css added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+import { RoughNotation } from "react-rough-notation";
 
 function About() {
   return (
@@ -14,17 +14,17 @@ function About() {
         <p>I love to combine beauty with functionality.</p>
         <p>
           I'm a full stack web developer with a knowledge of{" "}
-          <RoughNotation type="highlight" show={true} color="#7FB7BE70" iterations={1}>React</RoughNotation>, <RoughNotation type="highlight" show={true} color="#FFF7AE" iterations={1}>JavaScript</RoughNotation>
-          , and <RoughNotation type="highlight" show={true} color="#DB7F6760" iterations={1}>CSS</RoughNotation>. I became fascinated with computer
+          <RoughNotation type="highlight" show={true} color="#90BAAD95" iterations={1}>React</RoughNotation>, <RoughNotation type="highlight" show={true} color="#FFB10070" iterations={1}>JavaScript</RoughNotation>
+          , and <RoughNotation type="highlight" show={true} color="#FFB10070" iterations={1}>CSS</RoughNotation>. I became fascinated with computer
           programming when I saw those first two words "Hello World" I had coded
           appear on screen. I had to learn more.
         </p>
         <p>
           I have a <RoughNotation type="highlight" show={true} color="rgb(244, 214, 244)" iterations={1}>strong growth mindset </RoughNotation>and a passion for creating. I'm
-          determined to see things through and <RoughNotation type="highlight" show={true} color="#7FB7BE70" iterations={1}>enjoy lifting teams to success </RoughNotation>with my positivity and kindness.
+          determined to see things through and <RoughNotation type="highlight" show={true} color="#90BAAD95" iterations={1}>enjoy lifting teams to success </RoughNotation>with my positivity and kindness.
         </p>
         <p>
-          I'm currently looking for a new role as a developer. <RoughNotation type="circle" show={true} color="#DB7F6760" iterations={4}>So let's talk!</RoughNotation>
+          I'm currently looking for a new role as a developer. <RoughNotation type="circle" show={true} color="rgb(244, 214, 244)" iterations={10}>So let's talk!</RoughNotation>
         </p>
         <button
           className="github-button"
