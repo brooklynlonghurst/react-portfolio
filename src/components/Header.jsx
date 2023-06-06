ec72b74218b6ca486a9fdf1b7b2e381ae84c0ba3
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-06-06 14:36:16 -0600

    adds resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ function Header() {
         <a class="about-tag" href="#intro-container">About</a>
         <a class="projects-tag" href="#projects-section">Projects</a>
         <a class="contact-tag" href="#contact-section">Contact</a>
+        <a class="contact-tag" href='https://docs.google.com/document/d/1LQA8-r6B4Y_Oid-4jiG-tXKHBK6uygWBKr0EVLEG-PA/edit?usp=sharing'>Resume</a>
         </div>
     </div>
     )
